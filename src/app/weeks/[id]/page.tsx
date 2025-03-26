3c7741812cf5956c47c9be2c5b102b49eb06584f
Artjom03 <artjom.kuzmenko03@gmail.com>
2025-03-26 12:32:32 +0100

    test

--- src/app/weeks/[id]/page.tsx
+++ src/app/weeks/[id]/page.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link'
 import { notFound } from 'next/navigation'
-import WeekSidebar from '@/app/components/WeekSidebar'
 import WeekDays from '@/app/components/WeekDays'
 
 interface WeekData {
